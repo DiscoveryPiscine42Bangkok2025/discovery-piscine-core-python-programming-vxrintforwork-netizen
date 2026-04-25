7db423b5e3e9dd62624e3859d965c2ca0e8d8637
vxrintforwork-netizen <vxrintforwork@gmail.com>
2026-04-25 13:26:41 +0700

    Add files via upload

---
+++ cell02/mult.py
@@ -0,0 +1,9 @@
+number1=int(input("Please enter the first number:"))
+number2=int(input("Please enter the second number:"))
+result=number1 * number2
+if result>0:
+    print("The result is positive.")
+elif result==0:
+    print("The result is zero.")
+else:
+    print("The result is negative")
