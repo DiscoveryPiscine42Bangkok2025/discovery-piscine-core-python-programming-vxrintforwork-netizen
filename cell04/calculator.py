132dd4dccb8a1b3328bc05141c55d493670a351b
vxrintforwork-netizen <vxrintforwork@gmail.com>
2026-04-26 13:18:27 +0700

    Add files via upload

---
+++ cell04/calculator.py
@@ -0,0 +1,7 @@
+number_1=int(input("Give me the first number:"))
+number_2=int(input("Give me the second number:"))
+print("Thank you!")
+print(f"{number_1} + {number_2} = {number_1 + number_2}")
+print(f"{number_1} - {number_2} = {number_1 - number_2}")
+print(f"{number_1} / {number_2} = {number_1 / number_2}")
+print(f"{number_1} * {number_2} = {number_1 * number_2}")
